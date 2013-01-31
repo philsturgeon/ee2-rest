853cf1b32475398553571b9e4402da6a75b79c14
Phil Sturgeon <email@philsturgeon.co.uk>
2013-01-31 08:33:28 -0500

    Missing new keyword.

--- mcp.rest.php
+++ mcp.rest.php
@@ -31,7 +31,7 @@ class Rest_mcp
 		define('REST_PARAMS', 'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module='.$this->module_name);
 		define('REST_URL', BASE.AMP.REST_PARAMS);
 
-		$this->data = stdClass;
+		$this->data = new stdClass;
 		$this->data->base = REST_URL;
 
 		$this->EE->load->library('form_validation');
